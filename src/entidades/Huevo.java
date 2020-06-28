9c8bb972d0f48d0be6d383adbaa9cfebebc8a43e
Fernanda Arenas <fernanda.arenas.diaz@gmail.com>
2020-06-27 20:26:41 -0400

    Se agrega el tiro, el puntaje y el cambio de caracter en la matriz

--- src/entidades/Huevo.java
+++ src/entidades/Huevo.java
@@ -1,10 +1,15 @@
 package entidades;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import negocios.Tablero;
 public class Huevo {
 
 	private int filaHuevo;
 	private int columnaHuevo;
 	private int puntajeLanzamiento;
+
 	/**
 	 * @param filaHuevo
 	 * @param columnaHuevo
@@ -16,47 +21,110 @@ public class Huevo {
 		this.columnaHuevo = columnaHuevo;
 		this.puntajeLanzamiento = puntajeLanzamiento;
 	}
+
+	public Huevo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	public int pedirCoordenadaFila() {
+		int sw=1;
+		
+	
+		do {
+		String tecladoFila = JOptionPane.showInputDialog("Introduce la coordenada de la Fila: ");
+		filaHuevo = Integer.parseInt(tecladoFila);
+		
+		if (validarCoordenada(filaHuevo) != false) {
+			sw=0;
+		}
+		}while (sw==1);
+	
+	
+		
+		
+	return filaHuevo;
+    
+    }
+//fin pedir coordenada fila
+	public int pedirCoordenadaColumna() {
+		
+		int sw2=1;
+	
+				
+		do {
+		
+		String tecladoColumna = JOptionPane.showInputDialog("Introduce la coordenada de la Columna: ");
+		columnaHuevo = Integer.parseInt(tecladoColumna);
+		if (validarCoordenada(columnaHuevo) != false) {
+			sw2=0;
+		}
+		}while (sw2==1);
+	
+		
+		
+	return columnaHuevo;
+    
+    }
+
+	
+/////fin prdir cor columna
+	public boolean validarCoordenada(int num) {
+		if (num >= 0 && num <= 14) {
+			return true;
+		} else
+			return false;
+	}
+
 	/**
 	 * @return the filaHuevo
 	 */
 	public int getFilaHuevo() {
 		return filaHuevo;
 	}
+
 	/**
 	 * @return the columnaHuevo
 	 */
 	public int getColumnaHuevo() {
 		return columnaHuevo;
 	}
+
 	/**
 	 * @return the puntajeLanzamiento
 	 */
 	public int getPuntajeLanzamiento() {
 		return puntajeLanzamiento;
 	}
+
 	/**
 	 * @param filaHuevo the filaHuevo to set
 	 */
 	public void setFilaHuevo(int filaHuevo) {
 		this.filaHuevo = filaHuevo;
 	}
+
 	/**
 	 * @param columnaHuevo the columnaHuevo to set
 	 */
 	public void setColumnaHuevo(int columnaHuevo) {
 		this.columnaHuevo = columnaHuevo;
 	}
+
 	/**
 	 * @param puntajeLanzamiento the puntajeLanzamiento to set
 	 */
 	public void setPuntajeLanzamiento(int puntajeLanzamiento) {
 		this.puntajeLanzamiento = puntajeLanzamiento;
 	}
+
 	@Override
 	public String toString() {
 		return "Huevo [filaHuevo=" + filaHuevo + ", columnaHuevo=" + columnaHuevo + ", puntajeLanzamiento="
 				+ puntajeLanzamiento + "]";
 	}
-	
+
 	
 }
--- src/negocios/Tablero.java
+++ src/negocios/Tablero.java
@@ -1,13 +1,13 @@
 package negocios;
+
 import java.util.ArrayList;
 import java.util.Random;
 
 import entidades.Carro;
 import entidades.Huevo;
 
-
 public class Tablero {
-	
+
 	// Creación Atributos Matriz
 	char matrizEscenario[][] = new char[15][15];
 	int filaKromis;
@@ -16,23 +16,23 @@ public class Tablero {
 	int columnaCaguano;
 	int filaTrupalla;
 	int columnaTrupalla;
-	
+
 	Random generadorNumeroAleatorio = new Random();
-	
-	private ArrayList <Object> carroList;  // Arreglo de 18 carros
-	private ArrayList <Huevo> tirosHuevos; // Arreglo de tiros de huevos
+
+	private ArrayList<Object> carroList; // Arreglo de 18 carros
+	private ArrayList<Huevo> tirosHuevos; // Arreglo de tiros de huevos
+
 	/**
 	 * @param carroList
 	 * @param tirosHuevos
 	 */
 
-	
-	
 	public void crearCarro() {
-		// crear subclase de la clase carro, y asignar a la lista respectiva carroList o tirosHuevo
-		
+		// crear subclase de la clase carro, y asignar a la lista respectiva carroList o
+		// tirosHuevo
+
 	}
-	
+
 	// Constructor de las posiciones
 	public Tablero(int filaKromis, int columnaKromis, int filaCaguano, int columnaCaguano, int filaTrupalla,
 			int columnaTrupalla) {
@@ -45,18 +45,41 @@ public class Tablero {
 		this.columnaTrupalla = -1;
 	}
 
+	public Tablero() { // Constructor vacio
+
+	}
+
+	public void mostrarMatriz() {
+		for (int x = 0; x < 15; x++) {
+			System.out.print("\n");
+			for (int y = 0; y < 15; y++) {
+				if (y != 15) {
+					System.out.print(matrizEscenario[x][y]);
+
+				}
+
+				System.out.print(" ");
+			}
+
+		}
+	}
+
 	public void inicializarMatriz() {
 		creacionMatrizVacia();
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 3; i++) {
 			posicionKromis();
-			
-			
-			
 		}
-		
+
+		for (int j = 0; j < 5; j++) {
+			posicionCaguano();
+		}
+
+		for (int z = 0; z < 10; z++) {
+			posicionTrupalla();
+		}
+
 	}
-	
-	
+
 	public void creacionMatrizVacia() {
 		for (int x = 0; x < 15; x++) {
 			System.out.print("\n");
@@ -65,16 +88,16 @@ public class Tablero {
 			}
 		}
 	}
-	
+
 	public void posicionKromis() {
 		int switchKromi = 1;
 
 		do {
-			filaKromis = generadorNumeroAleatorio.nextInt(15); // Aleatorio
+			filaKromis = generadorNumeroAleatorio.nextInt(13); // Aleatorio
 			columnaKromis = generadorNumeroAleatorio.nextInt(15); // Aleatorio
-			
+
 			while (filaKromis > 13) { // Validar que la fila debe ser menor a 13
-				filaKromis = generadorNumeroAleatorio.nextInt(15);
+				filaKromis = generadorNumeroAleatorio.nextInt(13);
 			}
 			// Validar espacios vacios
 			if (matrizEscenario[filaKromis][columnaKromis] == '*') {
@@ -84,46 +107,47 @@ public class Tablero {
 						matrizEscenario[filaKromis + 1][columnaKromis] = 'K'; // Asigna la posición carro Kromi
 						matrizEscenario[filaKromis + 2][columnaKromis] = 'K'; // Asigna la posición carro Kromi
 						switchKromi = 0; // Salida del ciclo
+
 					}
 				}
 			}
 
 		} while (switchKromi == 1);
 	}
-		
+
 	public void posicionCaguano() {
-			int switchCaguano = 1;
-
-			do {
-				filaCaguano = generadorNumeroAleatorio.nextInt(15);
-				columnaCaguano = generadorNumeroAleatorio.nextInt(15);
-
-				while (filaCaguano > 15) { // Validar que la fila debe ser menor a 15
-					filaCaguano = generadorNumeroAleatorio.nextInt(15);
-				}
-				// Validar Columna
-				while (columnaCaguano >= 14) {
-					columnaCaguano = generadorNumeroAleatorio.nextInt(15);
-				}
-
-				// Validar Espacios Vacios
-				if (matrizEscenario[filaCaguano][columnaCaguano] == '*') {
-					if (matrizEscenario[filaCaguano][columnaCaguano + 1] == '*') { // Validar espacio vacio del costado
-						matrizEscenario[filaCaguano][columnaCaguano] = 'C'; // Posicionar Caguano
-						matrizEscenario[filaCaguano][columnaCaguano + 1] = 'C';
-						switchCaguano = 0;
-					}
-				}
-
-			} while (switchCaguano == 1); // Fin Validación
-		}
-	
-	public void posicionTrupalla() {
-	
-			int switchTrupalla = 1;
+		int switchCaguano = 1;
 
 		do {
-			filaTrupalla = generadorNumeroAleatorio.nextInt(15); //Aleatorio
+			filaCaguano = generadorNumeroAleatorio.nextInt(15);
+			columnaCaguano = generadorNumeroAleatorio.nextInt(14);
+
+			while (filaCaguano > 15) { // Validar que la fila debe ser menor a 15
+				filaCaguano = generadorNumeroAleatorio.nextInt(15);
+			}
+			// Validar Columna
+			while (columnaCaguano >= 14) {
+				columnaCaguano = generadorNumeroAleatorio.nextInt(14);
+			}
+
+			// Validar Espacios Vacios
+			if (matrizEscenario[filaCaguano][columnaCaguano] == '*') {
+				if (matrizEscenario[filaCaguano][columnaCaguano + 1] == '*') { // Validar espacio vacio del costado
+					matrizEscenario[filaCaguano][columnaCaguano] = 'C'; // Posicionar Caguano
+					matrizEscenario[filaCaguano][columnaCaguano + 1] = 'C';
+					switchCaguano = 0;
+				}
+			}
+
+		} while (switchCaguano == 1); // Fin Validación
+	}
+
+	public void posicionTrupalla() {
+
+		int switchTrupalla = 1;
+
+		do {
+			filaTrupalla = generadorNumeroAleatorio.nextInt(15); // Aleatorio
 			columnaTrupalla = generadorNumeroAleatorio.nextInt(15);
 
 			// Validar que la fila debe ser menor a 14
@@ -134,7 +158,7 @@ public class Tablero {
 			while (columnaTrupalla > 15) {
 				columnaTrupalla = generadorNumeroAleatorio.nextInt(15);
 			}
-			// Validar Espacios Vacios 
+			// Validar Espacios Vacios
 			if (matrizEscenario[filaTrupalla][columnaTrupalla] == '*') {
 				matrizEscenario[filaTrupalla][columnaTrupalla] = 'T';
 				switchTrupalla = 0;
@@ -143,17 +167,11 @@ public class Tablero {
 		} while (switchTrupalla == 1);
 	}
 
-		
-	
 	public void lanzarHuevo() {
-		// crear instancia clase huevo, solicitar coordenadas, calcular el puntaje y almacenar
+		// crear instancia clase huevo, solicitar coordenadas, calcular el puntaje y
+		// almacenar
 	}
-	
-	public void mostrarMatriz() {
-		// iterar carrolist e iterar en la matriz, con huevos igual, mostrar foto de la matriz
-			
-	}
-	
+
 	public void calcularPuntaje() {
 		lanzarHuevo();
 	}
@@ -190,7 +208,25 @@ public class Tablero {
 	public String toString() {
 		return "Tablero [carroList=" + carroList + ", tirosHuevos=" + tirosHuevos + "]";
 	}
+
+public int puntajeHuevo(int fila, int columna,int acomulador) {
 		
+		char posicion=matrizEscenario[fila][columna];
+		
+		if (posicion=='*'|| posicion=='H'){
+			acomulador=acomulador+0;
+		}else if (posicion=='C'){
+			   acomulador=acomulador+2;
+		}else if (posicion=='T'){
+			   acomulador=acomulador+1;	   
+		}else if (posicion=='K'){
+			   acomulador=acomulador+3;
+		
+		}	
+	matrizEscenario[fila][columna]='H';
+	
+		
+	return acomulador;}
 	
 
 }
